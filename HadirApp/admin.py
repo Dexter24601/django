0511db2e9af291d13a4ed2371533b0754db3f28e
dexter <80349411+Dexter24601@users.noreply.github.com>
2023-02-07 18:06:06 +0300

    Adding everyting

---
+++ HadirApp/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import Student, Class, Image, Attendance, Absence, Traning
+
+admin.site.register(Student)
+admin.site.register(Class)
+admin.site.register(Image)
+admin.site.register(Attendance)
+admin.site.register(Absence)
+admin.site.register(Traning)
